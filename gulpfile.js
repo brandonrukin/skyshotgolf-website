75403c054528eda8bc507384a7bd9e296f6d75c3
Brandon Rukin <rukin.brandon@gmail.com>
2016-01-11 12:45:20 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+//require gulp
+var gulp = require('gulp');
+
+//require gulp plugins
+var postcss = require('gulp-postcss'),
+   cssnext = require('cssnext'),
+   precss = require('precss'),
+   autoprefixer = require('autoprefixer-core'),
+   lost = require('lost'),
+   rucksack = require('rucksack-css'),
+   sourcemaps = require('gulp-sourcemaps'),
+   browserSync = require('browser-sync'),
+   imagemin = require('gulp-imagemin');
+
+gulp.task('styles', function() {
+   var processors = [
+      precss({}),
+      lost,
+      rucksack,
+      autoprefixer({browsers:['last 2 version']}),
+      cssnext({})
+   ];
+
+   return gulp.src('src/css/styles.css')
+      .pipe(postcss(processors))
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./dist/css'))
+      .pipe(browserSync.reload({
+         stream: true
+      }));
+}); //styles task
+
+gulp.task('images', function() {
+   gulp.src('src/images/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('./dist/images'));
+}); //images task
+
+gulp.task('browserSync', function() {
+   browserSync({
+      server: {
+         baseDir: './'
+      },
+      notify: false
+   })
+}); //browserSync task
+
+gulp.task('default', ['styles', 'browserSync'], function() {
+   gulp.watch('**/*.css', ['styles']);
+   gulp.watch('**/*.html', browserSync.reload);
+}); //watch task
+
+
